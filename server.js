3f1a5382422b866e3fac562665160feab8d65571
DSantos29 <sdaniloscofield@gmail.com>
2023-01-25 12:46:39 -0300

    Mudando route

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+// server.js
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-server.use(router)
 server.use(middlewares)
+server.use(router)
 
-server.listen(3000, () => { console.log('JSON Server está rodando') })
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
